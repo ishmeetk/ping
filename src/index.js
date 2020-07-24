bbc82eca516cde78781031518812a414ac21bc7f
Nischay Modi <modinischay1@gmail.com>
2020-07-24 15:54:29 -0400

    Changed to default react app

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
+//import Counter from "./components/counter";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <App /> 
+    {/*Counter*/}
   </React.StrictMode>,
   document.getElementById("root")
 );
