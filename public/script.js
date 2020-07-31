fa31627482c22df87ce6e4ba724098813e6ec279
Nischay Modi <nischay7@DESKTOP-KALI.T-mobile.com>
2020-07-31 16:21:50 -0400

    Same files

--- public/script.js
+++ public/script.js
@@ -1,60 +1,83 @@
-const socket = io('/')
-const videoGrid =  document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-    host: '/',
-    port: '3001'
-})
+let user;
 
-const myVideo = document.createElement('video')
-myVideo.muted = true
-const peers = {}
+document.addEventListener("DOMContentLoaded", init)
 
-navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-}).then(stream => {
-    addVideoStream(myVideo, stream)
+class User {
+    static videoGrid = document.getElementById('video-grid')
+    static peers = {}
 
-    myPeer.on('call', call => {
-        call.answer(stream)
+    constructor() {
+        this.socket = io('/')
+        
+        this.myPeer = new Peer(undefined, {
+            host: '/',
+            port: '3001'
+        })
+        
+        this.myVideo = document.createElement('video')
+        this.myVideo.muted = true
+    }
 
+    connectToNewUser (userId, stream) {
+        const call = this.myPeer.call(userId, stream)
         const video = document.createElement('video')
+
         call.on('stream', userVideoStream => {
             addVideoStream(video, userVideoStream)
         })
-    })
-    socket.on('user-connected', userId => {
-        connectToNewUser(userId, stream)
-    })
-})
+        call.on('close', () => {
+            video.remove()
+        })
 
-socket.on('user-disconnected', userId => {
-    if(peers[userId])
-        peers[userId].close()
-
-})
-
-myPeer.on('open', id => {
-    socket.emit('join-room', ROOM_ID, id)  
-})
-
-function connectToNewUser (userId, stream) {
-    const call = myPeer.call(userId, stream)
-    const video = document.createElement('video')
-
-    call.on('stream', userVideoStream => {
-        addVideoStream(video, userVideoStream)
-    })
-    call.on('close', () => {
-        video.remove()
-    })
-
-    peers[userId] = call
+        User.peers[userId] = call
+    }
 }
+
 function addVideoStream(video, stream) {
     video.srcObject = stream
     video.addEventListener('loadedmetadata', () => {
         video.play()
+
     })
-    videoGrid.append(video)
+    User.videoGrid.append(video)
 }
+
+function init() {
+    const joinRoomBtn = document.getElementById("join-room");
+    joinRoomBtn.addEventListener("click", function() {
+        user = new User()
+
+        // get user's video/audio
+        navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: false
+        }).then(stream => {
+            addVideoStream(user.myVideo, stream)
+
+            user.myPeer.on('call', call => {
+                call.answer(stream)
+
+                const video = document.createElement('video')
+                call.on('stream', userVideoStream => {
+                    addVideoStream(video, userVideoStream)
+                })
+            })
+            user.socket.on('user-connected', userId => {
+                user.connectToNewUser(userId, stream)
+            })
+
+        })
+
+        user.socket.on('user-disconnected', userId => {
+            if(User.peers[userId])
+                User.peers[userId].close()
+        })
+
+        user.myPeer.on('open', id => {
+            user.socket.emit('join-room', ROOM_ID, id)  
+        })
+
+        joinRoomBtn.classList.add("hidden");
+    })
+}
+
--- server.js
+++ server.js
@@ -4,26 +4,31 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 
- app.set('view engine', 'ejs')
- app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
- app.get('/', (req, res) => {
-    res.redirect(`/${uuidV4()}`)    
- })
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
 
- app.get('/:room', (req, res) => {
-    res.render('room', { roomId: req.params.room })
- })
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
 
- io.on('connection', socket => {
-     socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId)
-        socket.to(roomId).broadcast.emit('user-connected', userId)
+io.on('connection', socket => { // STEP 1.1
+  socket.on('join-room', (roomId, userId) => { // STEP 2.2
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId) // STEP 2.3
 
-        socket.on('disconnect', () => {
-            socket.to(roomId).broadcast.emit('user-disconnected', userId)
-        })
-     }) 
- })
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId) // STEP 4
+    })
 
-server.listen(5000)
+    // TODO
+    socket.on('pingUser', (data) => {
+      // socket.to(roomId).broadcast.emit('start-ping', )
+    })
+  })
+})
+
+server.listen(process.env.PORT || 3000)
